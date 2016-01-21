b0a67ed21d4529d08a56aae3c5a9d4f435fb5ed3
zvovov <zvovov@gmail.com>
2016-01-21 14:45:15 +0530

    functional style

--- webdown.py
+++ webdown.py
@@ -43,12 +43,26 @@ def get_script(soup):
     script_tags = soup.find_all('script')
     print("<script> TAGS")
     for _ in script_tags:
-        print(_['src'])
+        print(type(_))
+        # print(_.get(['src']))
 
-if __name__ == '__main__':
-    
-    print('\n--- Webpage Asset Viewer ---\n')
+def interface(soup):
+    """
+    User Friendly interface
+    """
+    choice = input("""Press 1, 2, or 3. Then press Enter:
+                    1. Show all hyperlinks on the webpage.
+                    2. Show all css files on the webpage.
+                    3. Show all javascript files on the webpage.
+                    4. Enter a new URL.
+                    5. Exit. 
+                    """)
+    print(choice)
 
+def initializer():
+    """
+    Prompts user to enter URL and diplays interface.
+    """
     entered_url = input('Enter website url: ').strip()
 
     # Add 'http://' if not present
@@ -62,16 +76,22 @@ if __name__ == '__main__':
 
         soup = BeautifulSoup(r.text, 'html.parser')
         #print(soup)
-
+        
+        # interface(soup)
         # <link> tags
-        get_link(soup)
+        # get_link(soup)
 
-        # <a> tags
-        get_a(soup)
+        # # <a> tags
+        # get_a(soup)
 
         # <script> tags
         get_script(soup)
 
     else:
         # More than 3 dots in URL
-        exit('Error 1: Invalid URL')    
+        exit('Error 1: Invalid URL')
+
+if __name__ == '__main__':
+    
+    print('\n--- Webpage Asset Viewer ---\n')
+    initializer()
